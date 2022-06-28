47b63e0aacd79a6e48b47935a70b27b6a0d4f717
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-06-28 09:23:28 +0430

    Finally this project has been done.

--- script.js
+++ script.js
@@ -10,84 +10,84 @@ const Questions = [{
 },
 {
     id: 1,
-    q: "Tehran is the capital of ... ?",
-    a: [{ text: "Thailand", isSelected: false },
-        { text: "Iraq", isCorrect: false },
-        { text: "Canada", isCorrect: false },
-        { text: "Iran", isCorrect: true }
+    q: "1 * 5 ?",
+    a: [{ text: "6", isSelected: false },
+        { text: "7", isCorrect: false },
+        { text: "4", isCorrect: false },
+        { text: "5", isCorrect: true }
     ]
 
 },
 {
     id: 2,
-    q: "What is the capital of Gujarat",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "6 + 9 ?",
+    a: [{ text: "14", isCorrect: false },
+        { text: "15", isCorrect: true },
+        { text: "17", isCorrect: false },
+        { text: "16", isCorrect: false }
     ]
 },
 {
     id: 3,
-    q: "What is the capital of Gujarat1",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "9 * 11 ?",
+    a: [{ text: "99", isCorrect: true },
+        { text: "98", isCorrect: false },
+        { text: "999", isCorrect: false },
+        { text: "9", isCorrect: false }
     ]
 },
 {
     id: 4,
-    q: "What is the capital of Gujarat2",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "7 / 1 ?",
+    a: [{ text: "14", isCorrect: false },
+        { text: "77", isCorrect: false },
+        { text: "3.5", isCorrect: false },
+        { text: "7", isCorrect: true }
     ]
 },
 {
     id: 5,
-    q: "What is the capital of Gujarat3",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "4 * 7 ?",
+    a: [{ text: "24", isCorrect: false },
+        { text: "28", isCorrect: true },
+        { text: "32", isCorrect: false },
+        { text: "36", isCorrect: false }
     ]
 },
 {
     id: 6,
-    q: "What is the capital of Gujarat4",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "1 * 25 ?",
+    a: [{ text: "1/25", isCorrect: false },
+        { text: "2.5", isCorrect: false },
+        { text: "25", isCorrect: true },
+        { text: "1.25", isCorrect: false }
     ]
 },
 {
     id: 7,
-    q: "What is the capital of Gujarat5",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "65 / 5 ?",
+    a: [{ text: "325", isCorrect: false },
+        { text: "14", isCorrect: false },
+        { text: "15", isCorrect: false },
+        { text: "13", isCorrect: true }
     ]
 },
 {
     id: 8,
-    q: "What is the capital of Gujarat6",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "8 * 9 ?",
+    a: [{ text: "72", isCorrect: true },
+        { text: "81", isCorrect: false },
+        { text: "90", isCorrect: false },
+        { text: "100", isCorrect: false }
     ]
 },
 {
     id: 9,
-    q: "What is the capital of Gujarat7",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "100 - 8 ?",
+    a: [{ text: "93", isCorrect: false },
+        { text: "108", isCorrect: false },
+        { text: "92", isCorrect: true },
+        { text: "91", isCorrect: false }
     ]
 }
 ]
@@ -126,19 +126,19 @@ function iterate(id) {
     var selected = "";
 
     op1.addEventListener("click", () => {
-        selected = op1.innerText;
+        selected = op1.value;
     })
   
     op2.addEventListener("click", () => {
-        selected = op2.innerText;
+        selected = op2.value;
     })
   
     op3.addEventListener("click", () => {
-        selected = op3.innerText;
+        selected = op3.value;
     })
   
     op4.addEventListener("click", () => {
-        selected = op4.innerText;
+        selected = op4.value;
     })
 
 
@@ -154,20 +154,47 @@ function iterate(id) {
             result[0].style.color = "red";
         }
     })
+}
+
+if (start) {
+    iterate("0");
+}
+
+function push1() {
+    const op1 = document.getElementById('op1');
+    answers.push(op1.innerHTML);
+}
+
+function push2() {
+    const op2 = document.getElementById('op2');
+    answers.push(op2.innerHTML);
+}
+
+function push3() {
+    const op3 = document.getElementById('op3');
+    answers.push(op3.innerHTML);
+}
+
+function push4() {
+    const op4 = document.getElementById('op4');
+    answers.push(op4.innerHTML);
+}
+
+function ShowResults(){
+    var text = "";
+    for (let i = 0; i < answers.length; i++) {
+        text += "your answer for question " + i + " is:  " + answers[i] + "\n"; 
     }
+    alert(text);
+}
 
-    if (start) {
-        iterate("0");
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+next.addEventListener("click", () => {
+    start = false;
+    if (id < 10) {
+        id++;
+        iterate(id);
     }
-
-    const next = document.getElementsByClassName('next')[0];
-    var id = 0;
-    next.addEventListener("click", () => {
-        start = false;
-        if (id < 10) {
-            id++;
-            iterate(id);
-        }
-
-    })
+})
     
